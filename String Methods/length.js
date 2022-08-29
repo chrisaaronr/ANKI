be0d32fd4dc9e1f3930655bcd72dabc355710494
Christopher Aaron <christops.codes@gmail.com>
2022-08-28 17:59:44 -0700

    delete file

--- String Methods/length.js
+++
@@ -1,8 +0,0 @@
-// .length -- Finding the length of the string
-
-//The most important string method and the most commonly used one.  Calling `.length` on a string will return the number of character(s) that the string contains.
-
-//Example
-const str = "this string has 29 characters";
-console.log(str.length) // 29
-
